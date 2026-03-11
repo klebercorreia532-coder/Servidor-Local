f005639ee98f164025013397945aedfcdf149112
Kleber Veiga <Klebercorreia532@gmail.com>
2026-03-11 12:36:52 -0100

    adicionar comicao com base de dados

---
+++ src/lib/db.ts
@@ -0,0 +1,10 @@
+import mysql from "mysql2/promise";
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "Baixadabxd123#",
+    database: "servico_local"
+});
+
+export default db;
