a7145c3caa2b8aa2791db5ae08f6bd55f53c281e
Kleber Veiga <Klebercorreia532@gmail.com>
2026-03-30 12:43:55 -0100

    toke-jwt

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -3,6 +3,8 @@
 import type { Request, Response } from "express"
 import type { UserType } from "../utils/types.js"
 import { UserModel } from "../models/users.model.js"
+import { comparePasswoerd } from "../utils/password.js"
+import jwt from "jsonwebtoken"
 
 export const UserController = {
     async create(req: Request, res: Response) {
@@ -51,7 +53,7 @@ export const UserController = {
                 data: null
             })
         }
- 
+
         const getUserByIdResponse = await UserModel.get(id as string)
 
         if (!getUserByIdResponse) {
@@ -106,10 +108,45 @@ export const UserController = {
             data: updateUserResponse
         })
     },
+    async login(req:Request, res:Response) {
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            return res.status(400).json({
+                status: "error",
+                message: "Dados de login invalidos",
+                data: null
+            })
+        }
+const userData = await UserModel.getByEmail(email as string)
+        if (!userData) {
+            return res.status(404).json({
+                status: "error",
+                message: "Utilizador nao encontrado",
+                data: null
+            })
+        }
+
+        const isPasswordValid = await comparePasswoerd(password, userData.password)
+        if (!isPasswordValid) {
+            return res.status(401).json({
+                status: "error",
+                message: "Credenciais invalidas",
+                data: null
+            })
+        }
+
+        const token = jwt.sign({ id: userData.id, email: userData.email }, process.env.JWT_SECRET as string, { expiresIn: "1h" })
+        return res.status(200).json({
+            status: "success",
+            message: "Login realizado com sucesso",
+            data: { token }
+        })
+    },
 
     async delete(req: Request, res: Response) {
         const { id } = req.params
-
+        
         if (!id) {
             return res.status(400).json({
                 status: "error",
@@ -117,9 +154,9 @@ export const UserController = {
                 data: null
             })
         }
-
+        
         const deleteUserResponse = await UserModel.delete(id as string)
-
+        
         if (!deleteUserResponse) {
             return res.status(400).json({
                 status: "error",
@@ -127,7 +164,7 @@ export const UserController = {
                 data: null
             })
         }
-
+        
         return res.status(200).json({
             status: "success",
             message: "Utilizador apagado com sucesso",
